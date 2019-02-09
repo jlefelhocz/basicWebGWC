539f9519b9538359f35d9f312bb78caa658966dc
jlefelhocz <jlefelhocz@yahoo.com>
2019-02-09 15:08:25 -0500

    Added CSS to make it pretty
    Also update javascript to change the header color to a prettier blue

--- assets/scripts/simple.js
+++ assets/scripts/simple.js
@@ -3,7 +3,7 @@ function changeHeader() {
     headerBlue = !headerBlue
     var x = document.getElementById("h2");
     if (headerBlue) {
-        x.style.color = "blue";
+        x.style.color = "#0074d9";
         x.innerHTML = "Surprise! I Changed the Header";
     } else {
         x.style.color = "black";
